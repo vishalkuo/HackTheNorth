12e795faa68400a95c21451a84e7e102f871417c
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-09-19 03:26:12 -0400

    Fix

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,7 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/api/open', function(req, res, next){
-    res.json(req.body.drawer)
+    console.log(req.body)
+    res.json({'drawer': req.body.drawer})
+})
+
+router.get('/api/test/open', function(req, res){
+    myObj = [{
+        test: 1
+    }]
+    res.json(myObj)
 })
 
 module.exports = router;
